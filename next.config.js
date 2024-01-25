4e748a2d6ce6900fd7bbb7e6cdecd60985d8a8c8
Fauzan <fauzanlubis23@gmail.com>
2024-01-25 13:16:29 +0800

    add react95

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
+const withFonts = require("next-fonts");
+const withTM = require("next-transpile-modules")(["react95"]);
+module.exports = withTM(withFonts());
